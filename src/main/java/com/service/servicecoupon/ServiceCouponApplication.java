bceb55087afe9b0a268f97b136022322acf7f7f9
jjeonmin <jms959310@gmail.com>
2024-06-18 14:57:38 +0900

    feat file add

---
+++ src/main/java/com/service/servicecoupon/ServiceCouponApplication.java
@@ -0,0 +1,15 @@
+package com.service.servicecoupon;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCouponApplication.class, args);
+	}
+
+}
