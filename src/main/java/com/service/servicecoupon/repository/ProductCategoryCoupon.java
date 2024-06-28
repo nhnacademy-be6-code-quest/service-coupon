e5ed2baa79bcbc518b8feb8f1355c977d9cb3dea
jjeonmin <jms959310@gmail.com>
2024-06-28 10:27:47 +0900

    feat: repository add

---
+++ src/main/java/com/service/servicecoupon/repository/ProductCategoryCoupon.java
@@ -0,0 +1,6 @@
+package com.service.servicecoupon.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductCategoryCoupon extends JpaRepository<ProductCategoryCoupon, Long> {
+}
