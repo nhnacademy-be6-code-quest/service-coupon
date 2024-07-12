3df03d9182225f3d5d3205737b9df0328815236e
jjeonmin <jms959310@gmail.com>
2024-07-12 12:52:28 +0900

    수정

--- src/main/java/com/service/servicecoupon/client/PaymentCouponClient.java
+++ src/main/java/com/service/servicecoupon/client/PaymentCouponClient.java
@@ -2,10 +2,12 @@ package com.service.servicecoupon.client;
 import com.service.servicecoupon.dto.request.CouponPaymentRewardRequestDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 @FeignClient(name = "paymentReward", url ="http://localhost:8001")
 public interface PaymentCouponClient {
 
-        @GetMapping("/api/client/birth-coupon")
-        CouponPaymentRewardRequestDto getUserPaymentValue();
+        @GetMapping("/api/coupon/payment/reward")
+        CouponPaymentRewardRequestDto getUserPaymentValue(@RequestBody CouponPaymentRewardRequestDto couponPaymentRewardRequestDto);
     }
 
